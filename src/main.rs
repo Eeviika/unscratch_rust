07744e05d6f64d793082ee187cee7847c88b39c6
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-17 14:05:26 -0400

    Made basic CLI test code in main()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod scratch;
 mod unscratch;
 
+use clap::Parser;
+use std::path::PathBuf;
+
+const LONG_ABOUT: &str = "A program / CLI tool to pack and unpack Scratch project files on your filesystem.\nAuthor: Eeviika";
+
+#[derive(Parser)]
+#[command(version, about, long_about = LONG_ABOUT)]
+struct CLI {
+    from: PathBuf,
+    to: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Unscratch is in DEVELOPMENT, bugs may occur...");
+    let cli = CLI::parse();
+    println!("Goodbye, world!")
 }
