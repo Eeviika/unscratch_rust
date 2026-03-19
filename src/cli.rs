544040db92b0b7c39669bf8f51712cf2cd7a9933
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-19 11:34:23 -0400

    Moved CLI related code to its own module

---
+++ src/cli.rs
@@ -0,0 +1,57 @@
+use clap::{Parser, Subcommand, ValueEnum};
+use std::path::{Path, PathBuf};
+
+const LONG_ABOUT: &str = "A program / CLI tool to pack and unpack Scratch project files on your filesystem.";
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum ProjectType {
+    Unscratch,
+    Scratch,
+}
+
+#[derive(Subcommand)]
+pub enum CommandType {
+    /// unpack a scratch project onto the filesystem
+    Unpack {
+        /// path to scratch project
+        #[arg(short, long)]
+        input: PathBuf,
+        /// path to folder to unpack the project to (will be created)
+        #[arg(short, long)]
+        output: Option<PathBuf>,
+        /// don't actually do anything
+        #[arg(long)]
+        dry_run: bool,
+    },
+    /// pack an unscratch project into a scratch project
+    Pack {
+        /// path to unpacked (unscratch) project
+        #[arg(short, long)]
+        input: PathBuf,
+        /// path to pack scratch project to
+        #[arg(short, long)]
+        output: Option<PathBuf>,
+        /// don't actually do anything
+        #[arg(long)]
+        dry_run: bool,
+    },
+    /// check a project for validity
+    Verify {
+        /// path to project
+        #[arg(short, long)]
+        input: PathBuf,
+        /// force a certain project type
+        #[arg(short,long)]
+        force_type: Option<ProjectType>
+    }
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = LONG_ABOUT, arg_required_else_help = true)]
+pub struct CLI {
+    /// Tell unscratch to print more info
+    #[arg(short, long)]
+    pub verbose: bool,
+    #[command(subcommand)]
+    pub command: CommandType,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,25 @@
 mod scratch;
 mod unscratch;
+mod cli;
 
 use clap::{Parser, Subcommand, ValueEnum};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use cli::*;
 
 const LONG_ABOUT: &str = "A program / CLI tool to pack and unpack Scratch project files on your filesystem.\nAuthor: Eeviika";
 
-#[derive(Debug, Clone, ValueEnum)]
-enum ProjectType {
-    Unscratch,
-    Scratch,
-}
+fn main() {
+    let cli = CLI::parse();
+    let is_verbose = cli.verbose;
 
-#[derive(Subcommand)]
-enum CommandType {
-    /// unpack a scratch project onto the filesystem
-    Unpack {
-        /// path to scratch project
-        #[arg(short, long)]
-        input: PathBuf,
-        /// path to folder to unpack the project to (will be created)
-        #[arg(short, long)]
-        output: Option<PathBuf>,
-        /// don't actually do anything
-        #[arg(long)]
-        dry_run: bool,
-    },
-    /// pack an unscratch project into a scratch project
-    Pack {
-        /// path to unpacked (unscratch) project
-        #[arg(short, long)]
-        input: PathBuf,
-        /// path to pack scratch project to
-        #[arg(short, long)]
-        output: Option<PathBuf>,
-        /// don't actually do anything
-        #[arg(long)]
-        dry_run: bool,
-    },
-    /// check a project for validity
-    Verify {
-        /// path to project
-        #[arg(short, long)]
-        input: PathBuf,
-        /// force a certain project type
-        #[arg(short,long)]
-        force_type: Option<ProjectType>
+    match cli.command {
+        CommandType::Unpack { input, output, dry_run } => unpack(input, output, dry_run),
+        _ => { todo!("Only Unpack is implemented at this time") }
     }
 }
 
-#[derive(Parser)]
-#[command(version, about, long_about = LONG_ABOUT, arg_required_else_help = true)]
-struct CLI {
-    /// Tell unscratch to print more info
-    #[arg(short, long)]
-    verbose: bool,
-    #[command(subcommand)]
-    command: CommandType,
-}
-
-fn main() {
-    println!("Unscratch is in DEVELOPMENT, bugs may occur...");
-    let cli = CLI::parse();
-    println!("Goodbye, world!")
+fn unpack(input: PathBuf, output: Option<PathBuf>, dry_run: bool) {
+    if !input.exists() {
+        panic!("input file must exist")
+    }
 }
