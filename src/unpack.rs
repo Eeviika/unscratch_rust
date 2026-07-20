b4458260e663e1c9e178e35d04561f2d650c458e
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-07-20 11:37:35 -0400

    added create_folders() and export_assets() to unpack.rs
    When unpacking, the tool will now create the proper output folders, and
    will also attempt to export assets (unless --no_assets is passed). This
    also adds std::fs, std::io, and std::path imports

--- src/unpack.rs
+++ src/unpack.rs
@@ -1,7 +1,12 @@
 use crate::cli::*;
 use crate::input_validator::are_filepaths_ok;
 use anyhow::{Ok, Result};
-use std::{fs::File, io::BufReader, path::PathBuf};
+use file_format::{FileFormat, Kind};
+use std::{
+    fs::{self, File},
+    io::{BufReader, Read, Seek, Write},
+    path::{Path, PathBuf},
+};
 use zip::ZipArchive;
 
 use crate::scratch::scratch_structs::*;
@@ -32,9 +37,19 @@ pub fn unpack(args: UnpackArgs, cli_options: CliOptions) -> Result<()> {
     let file = File::open(&input)?;
     let reader = BufReader::new(file);
 
+    create_folders(&output)?;
+
     println!("Scanning project file...");
 
     let mut archive = ZipArchive::new(reader)?;
+
+    if !no_assets {
+        let pb = indicatif::ProgressBar::new_spinner();
+        pb.set_message("Exporting assets...");
+        pb.enable_steady_tick(std::time::Duration::from_millis(100));
+        export_assets(&mut archive, &output)?;
+        pb.finish();
+    }
 
     let json_file = archive.by_name("project.json")?;
 
@@ -47,4 +62,89 @@ pub fn unpack(args: UnpackArgs, cli_options: CliOptions) -> Result<()> {
     pb.finish();
 
     Ok(())
+}
+
+fn create_folders(output: &Path) -> Result<()> {
+    if output.exists() {
+        fs::remove_dir_all(&output)?;
+    }
+
+    let assets = output.join(ASSETS_FOLDERNAME);
+    let sounds = assets.join(SOUNDS_FOLDERNAME);
+    let costumes = assets.join(COSTUMES_FOLDERNAME);
+    let sprites = output.join(SPRITES_FOLDERNAME);
+    let scripts = output.join(SCRIPTS_FOLDERNAME);
+
+    fs::create_dir_all(assets)?;
+    fs::create_dir_all(sounds)?;
+    fs::create_dir_all(costumes)?;
+    fs::create_dir_all(sprites)?;
+    fs::create_dir_all(scripts)?;
+    Ok(())
+}
+
+fn export_assets<R>(archive: &mut ZipArchive<R>, output_root: &Path) -> Result<()>
+where
+    R: Read + Seek,
+{
+    let mut valid_filenames = Vec::new();
+
+    let assets = output_root.join(ASSETS_FOLDERNAME);
+    let sounds = assets.join(SOUNDS_FOLDERNAME);
+    let costumes = assets.join(COSTUMES_FOLDERNAME);
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+
+        if file.is_dir() {
+            continue;
+        }
+
+        let mut bytes = Vec::new();
+        file.read_to_end(&mut bytes)?;
+
+        let format = FileFormat::from_bytes(&bytes);
+        let kind = format.kind();
+
+        if kind == Kind::Audio || kind == Kind::Image {
+            let name = file.name().to_owned();
+            // println!("{}", name);
+            valid_filenames.push(name);
+        }
+    }
+
+    for filename in valid_filenames {
+        let mut archive_file = archive.by_name(&filename)?;
+
+        let mut bytes = Vec::new();
+        archive_file.read_to_end(&mut bytes)?;
+
+        let format = FileFormat::from_bytes(&bytes);
+        let kind = format.kind();
+
+        let opt_path = archive_file.enclosed_name().to_owned();
+
+        if opt_path.is_none() {
+            continue;
+        }
+
+        let enclosed_path = opt_path.unwrap();
+        let opt_name = enclosed_path.file_name();
+
+        if opt_name.is_none() {
+            continue;
+        }
+
+        let enclosed_name = opt_name.unwrap();
+        let path = match kind {
+            Kind::Image => costumes.join(enclosed_name),
+            Kind::Audio => sounds.join(enclosed_name),
+            _ => assets.join(enclosed_name),
+        };
+
+        let mut asset_file = File::create(path)?;
+        asset_file.write_all(&bytes)?;
+    }
+
+    return Ok(());
 }
