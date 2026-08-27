6bc4b9ea01d927a8d4b9d8e8eab7de0a295ce07f
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-08-27 17:41:08 -0400

    add TryFrom for ScratchMonitor and MonitorMode

--- src/unscratch/monitor.rs
+++ src/unscratch/monitor.rs
@@ -45,3 +45,74 @@ pub struct SliderData {
     pub min: f32,
     pub max: f32,
 }
+
+impl TryFrom<&str> for MonitorMode {
+    type Error = Error;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "default" => Ok(MonitorMode::Default),
+            "large" => Ok(MonitorMode::Large),
+            "slider" => Ok(MonitorMode::Slider),
+            "list" => Ok(MonitorMode::List),
+            _ => bail!("Invalid value: {value}"),
+        }
+    }
+}
+
+impl TryFrom<ScratchMonitor> for Monitor {
+    type Error = Error;
+
+    fn try_from(value: ScratchMonitor) -> Result<Self, Self::Error> {
+        let ScratchMonitor {
+            id,
+            mode,
+            opcode,
+            params,
+            sprite_name,
+            value,
+            width,
+            height,
+            x,
+            y,
+            visible,
+            slider_min,
+            slider_max,
+            is_discrete,
+        } = value;
+
+        let id = Some(id);
+        let mode = MonitorMode::try_from(mode.as_str())?;
+
+        let general = GeneralData {
+            is_discrete,
+            sprite_name,
+            opcode,
+            params,
+            mode,
+        };
+
+        let looks = LooksData {
+            x,
+            y,
+            width,
+            height,
+            visible,
+        };
+
+        let mut slider: Option<SliderData> = None;
+
+        if slider_min.is_some() && slider_max.is_some() {
+            let min = slider_min.unwrap();
+            let max = slider_max.unwrap();
+
+            slider = Some(SliderData { min, max });
+        }
+
+        Ok(Monitor {
+            id,
+            general,
+            looks,
+            slider,
+        })
+    }
+}
